c1216f68866e7f78c2a4cb5a5793e539334e4fc8
Edward Chang <change@go.stockton.edu>
2019-11-15 14:33:52 -0500

    Oops, we didn't want this commit

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello
 {
 	public static void main(String argv[])
 	{
-		// Default is "World"
+		// Bad, but staged :(
 		String name = "World";
 		if(argv.length !=){
 			name = argv[0];
